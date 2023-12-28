55b6e357969ad94156dc71c5080617ae8e94d874
unknown <anhnguyen150301@gmail.com>
2023-12-29 00:43:40 +0700

    update services migrate

--- ASPNETDemo3/Program.cs
+++ ASPNETDemo3/Program.cs
@@ -17,13 +17,13 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 
-//using (var scope = app.Services.CreateScope())
-//{
-//    var services = scope.ServiceProvider;
+using (var scope = app.services.createscope())
+{
+    var services = scope.serviceprovider;
 
-//    var context = services.GetRequiredService<ApplicationDbContext>();
-//    context.Database.Migrate();
-//}
+    var context = services.getrequiredservice<applicationdbcontext>();
+    context.database.migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
